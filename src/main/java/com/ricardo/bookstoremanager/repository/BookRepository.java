cd6ceb52f04ed908b56df8a1678145efa556abd0
Ricardo <ricardo.miguel888@gmail.com>
2020-07-09 17:07:08 +0100

    Created book repository interface

---
+++ src/main/java/com/ricardo/bookstoremanager/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.ricardo.bookstoremanager.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ricardo.bookstoremanager.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long>{
+
+}
